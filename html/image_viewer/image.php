80fcd1b1120563e3da19609394ef3b69d10ac4e5
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2004-04-06 18:55:09 +0000

    PHP version of image viewer (uses very minimal javascript)

---
+++ html/image_viewer/image.php
@@ -0,0 +1,11 @@
+<?php
+
+
+$id = $_GET["id"];
+$width = $_GET["width"];
+$height = $_GET["height"];
+
+print "<center><img src=\"http://chaucer.library.emory.edu/iln/images/ILN$id.jpg\" width=\"$width\" height=\"$height\"></center>";
+
+
+?>
