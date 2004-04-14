987bb4a57478efb0436ea5b69c5706b378825598
rsutton <rsutton@32a9c6a3-583a-0410-a6b5-ea5aef07eef3>
2004-04-14 22:08:37 +0000

    added content - displays brief view of links from link database

--- trunk/html/links.php
+++ trunk/html/links.php
@@ -4,10 +4,39 @@ html_head("Links");
 
 include("xml/head.xml");
 include("xml/sidebar.xml");
+
+//$url = 'http://tamino.library.emory.edu/passthru/servlet/transform/tamino/BECKCTR/ILN?_xquery=
+$url = 'http://vip.library.emory.edu/tamino/BECKCTR/iln_links?_xquery=
+declare namespace dc="http://purl.org/dc/elements/1.1/"
+for $b in input()/link_collection/link_record
+return <link_record>
+ {$b/dc:title}
+ {$b/dc:description}
+ {$b/dc:identifier}
+</link_record>';
+
+$url = encode_url($url);
+$xsl_file = "links.xsl";
 ?>
 
    <div class="content"> 
           <h2>Links</h2>
+
+
+<?php
+
+print "<hr>";
+
+// use sablotron to transform xml
+$xmlContent = file_get_contents($url);
+$result = transform($xmlContent, $xsl_file); 
+print $result;
+
+print "<hr>";
+
+?> 
+
+
 
   </div>
    
