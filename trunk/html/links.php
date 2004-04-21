524445003c48f028e6d7e3360a3d31b16249024b
rsutton <rsutton@32a9c6a3-583a-0410-a6b5-ea5aef07eef3>
2004-04-21 21:12:51 +0000

    switched to use link_admin PHP object code

--- trunk/html/links.php
+++ trunk/html/links.php
@@ -1,12 +1,14 @@
 <?php
 include("common_functions.php");
+include_once ("link_admin/linkCollection.class.php");
+
 html_head("Links");
 
 include("xml/head.xml");
 include("xml/sidebar.xml");
 
 //$url = 'http://tamino.library.emory.edu/passthru/servlet/transform/tamino/BECKCTR/ILN?_xquery=
-$url = 'http://vip.library.emory.edu/tamino/BECKCTR/iln_links?_xquery=
+/*$url = 'http://vip.library.emory.edu/tamino/BECKCTR/iln_links?_xquery=
 declare namespace dc="http://purl.org/dc/elements/1.1/"
 for $b in input()/link_collection/link_record
 return <link_record>
@@ -16,7 +18,8 @@ return <link_record>
 </link_record>';
 
 $url = encode_url($url);
-$xsl_file = "links.xsl";
+$xsl_file = "links.xsl"; */
+
 ?>
 
    <div class="content"> 
@@ -28,9 +31,22 @@ $xsl_file = "links.xsl";
 print "<hr>";
 
 // use sablotron to transform xml
-$xmlContent = file_get_contents($url);
+/*$xmlContent = file_get_contents($url);
 $result = transform($xmlContent, $xsl_file); 
-print $result;
+print $result; */
+
+$sort = $_GET["sort"]; // options: title|contrib|date
+
+$args = array('host' => "vip.library.emory.edu",
+	      'db' => "BECKCTR",
+	      'coll' => 'iln_links',
+	      'sort' => $sort);
+
+$linkset = new LinkCollection($args);
+
+$linkset->printSortOptions("links.php");
+$linkset->printSummary();
+
 
 print "<hr>";
 
