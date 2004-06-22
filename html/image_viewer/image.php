216dd91f5e1397158cff6d03fbebdb3bfdee7d5d
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2004-06-22 19:37:30 +0000

    slight modification so fill height/fill width will work properly in all browsers

--- html/image_viewer/image.php
+++ html/image_viewer/image.php
@@ -5,7 +5,12 @@ $id = $_GET["id"];
 $width = $_GET["width"];
 $height = $_GET["height"];
 
-print "<center><img src=\"http://chaucer.library.emory.edu/iln/images/ILN$id.jpg\" width=\"$width\" height=\"$height\"></center>";
-
-
+print "<center><img src=\"http://chaucer.library.emory.edu/iln/images/ILN$id.jpg\"";
+if ($width != '') {
+   print " width=\"$width\" ";
+}
+if ($height != '') {
+  print " height=\"$height\" ";
+  }
+print "></center>";
 ?>
