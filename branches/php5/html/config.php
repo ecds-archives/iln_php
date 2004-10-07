a292dcd06d95d11985d6b7b6ec78e9071f56e78f
rsutton <rsutton@32a9c6a3-583a-0410-a6b5-ea5aef07eef3>
2004-10-07 21:59:34 +0000

    file with settings used by all pages (base url, tamino settings)

---
+++ branches/php5/html/config.php
@@ -0,0 +1,21 @@
+<?php
+
+/* Configuration settings for entire site */
+
+// set level of php error reporting -- turn off warnings when in production
+//error_reporting(E_ERROR | E_PARSE);
+
+
+// root directory and url where the website resides
+$server = "biliku.library.emory.edu";
+$base_path = "/rebecca/ilnweb";
+$base_url = "http://$server$base_path/";
+
+// add basedir to the php include path (for header/footer files and lib directory)
+set_include_path(get_include_path() . ":" . $basedir . ":" . "$basedir/lib");
+
+/* tamino settings  */
+$tamino_server = "vip.library.emory.edu";
+$tamino_db = "BECKCTR";
+$tamino_coll = "ILN";
+?>
