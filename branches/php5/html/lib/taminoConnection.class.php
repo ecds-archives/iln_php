c06da4f01b1dc9f0f4073f81c833cd23829cda84
rsutton <rsutton@32a9c6a3-583a-0410-a6b5-ea5aef07eef3>
2004-10-07 22:01:34 +0000

    added some error checking from ww1 version (handle gracefully when database does not respond at all); converting all xml-handling to new DOM XML from php5

--- branches/php5/html/lib/taminoConnection.class.php
+++ branches/php5/html/lib/taminoConnection.class.php
@@ -16,7 +16,11 @@ class taminoConnection {
   var $base_url;
   var $xmlContent;
   var $xml;
+  var $xpath;
   var $xsl_result;
+  var $xq_rval;
+  var $xq_code;
+  var $xq_msg;
 
   // cursor variables
   var $cursor;
@@ -54,28 +58,32 @@ class taminoConnection {
     }
 
     $this->xmlContent = file_get_contents($myurl);
-    if ($this->debug) {
-      $copy = $this->xmlContent;
-      $copy = str_replace(">", "&gt;", $copy);
-      $copy = str_replace("<", "\n&lt;", $copy);
-      print "DEBUG: in taminoConnection::xquery, xmlContent is <pre>$copy</pre>"; 
+    if ($this->xmlContent) {
+      $this->initializeXML();
+      if ($this->debug) {
+        $this->displayXML();
+      }
+      
+       if (!($this->xq_rval)) {    // tamino Error code (0 = success)
+         $this->getXQueryCursor();
+       } else if ($this->xq_rval == "8306") {
+       // invalid cursor position (also returned when there are no matches)
+         $this->count = $this->position = $this->quantity = 0;
+         if ($debug) {
+  	   print "DEBUG: Tamino error 8306 = invalid cursor position<br>\n";
+         }
+        } else if ($this->xq_rval) {
+           $this->count = $this->position = $this->quantity = 0;
+           print "<p>Error: failed to retrieve contents.<br>";
+           print "(Tamino error code $error)</p>";
+        }
+
+    } else {
+      print "<p><b>Error:</b> unable to access database.</p>";
+      $this->xq_rval = -1;
     }
 
-    $length = strlen($this->xmlContent);
-    if ($length < 5000) {
-      // phpDOM can only handle xmlContent within certain size limits
-      $this->xml = new XML($this->xmlContent);
-      if (!($this->xml)) {        ## call failed
-	print "TaminoConnection xquery Error: unable to retrieve xml content.<br>";
-      }
-      $error = $this->xml->getTagAttribute("ino:returnvalue", 
-					   "ino:response/ino:message");
-    } else {
-      // not really a tamino error.... might have unexpected results
-      $this->xml = 0;
-      $error = 0;
-    }
-   return $error;
+   return $this->xq_rval;
   }
 
 
@@ -86,41 +94,28 @@ class taminoConnection {
     if ($this->debug) {
       print "DEBUG: In function taminoConnection::xql, query is $query.<p>";
     }
-
     if (isset($position) && isset($maxdisplay)) {
       $xql = "_xql($position,$maxdisplay)=";
     } else {
       $xql = "_xql=";
     }
-
     $myurl = $this->base_url . $xql . $this->encode_xquery($query);
     if ($this->debug) {
       print "DEBUG: In function taminoConnection::xql, url is $myurl.<p>";
     }
-
     $this->xmlContent = file_get_contents($myurl);
-    if ($this->debug) {
-      $copy = $this->xmlContent;
-      $copy = str_replace(">", "&gt;", $copy);
-      $copy = str_replace("<", "\n&lt;", $copy);
-      print "DEBUG: in taminoConnection::xql, xmlContent is <pre>$copy</pre>"; 
-    }
 
-    $length = strlen($this->xmlContent);
-    if ($length < 150000) {
-      // phpDOM can only handle xmlContent within certain size limits
-      $this->xml = new XML($this->xmlContent);
-      if (!($this->xml)) {        ## call failed
-	print "TaminoConnection xquery Error: unable to retrieve xml content.<br>";
+    if ($this->xmlContent) {
+      $this->initializeXML();
+      if ($this->debug) {
+        $this->displayXML();
       }
-      $error = $this->xml->getTagAttribute("ino:returnvalue", 
-					   "ino:response/ino:message");
     } else {
-      // not really a tamino error.... might have unexpected results
-      $this->xml = 0;
-      $error = 0;
+      print "<p><b>Error:</b> unable to access database.</p>";
+      $this->xq_rval = -1;
     }
-   return $error;
+    
+    return $this->xq_rval;
   }
 
    // convert a readable xquery into a clean url for tamino
@@ -136,38 +131,48 @@ class taminoConnection {
    function getCursor () {
      // NOTE: this is an xql style cursor, not xquery
      if ($this->xml) {
-       $this->cursor = $this->xml->getBranches("ino:response", "ino:cursor");
-       if ($this->cursor) {
-	 $this->count = $this->cursor[0]->getTagAttribute("ino:count", "ino:cursor");
-       } else {
-	 // no matches (or, possibly-- unable to retrieve cursor)
-	 $this->count = 0;
-       }
+       $nl = $this->xpath->query("/ino:response/ino:cursor/@ino:count");
+       if ($nl) { $this->count = $nl->item(0)->textContent; }
      } else {
        print "Error! taminoConnection xml variable uninitialized.<br>";
      }
    }
 
-   // transform the tamino XML with a specified stylesheet
-   function xslTransform ($xsl_file, $xsl_params = NULL) { 
-     // create xslt handler
-     $xh = xslt_create();
-     // specify file base so that xsl includes will work
-     // Note: last / on end of fileBase is important!
-     $fileBase = 'file://' . getcwd () . "/xsl/";
-     //  print "file base is $fileBase<br>";
-     xslt_set_base($xh, $fileBase);
 
-     $args = array('/_xml' => $this->xmlContent);
-     $this->xsl_result = xslt_process($xh, 'arg:/_xml', $xsl_file, NULL, $args, $xsl_params);
-     
-     if ($this->xsl_result) {
-       // Successful transformation
+      // retrieve the XQuery style cursor & get the total count
+   function getXQueryCursor () {
+     if ($this->xml) {
+       $nl = $this->xpath->query("/ino:response/ino:cursor/ino:current/@ino:position");
+       if ($nl) {  $this->position = $nl->item(0)->textContent; }
+       $nl = $this->xpath->query("/ino:response/ino:cursor/ino:current/@ino:quantity");
+       if ($nl) { $this->quantity = $nl->item(0)->textContent; }
+
+       $total = $this->xml->getElementsByTagName("total");
+       if ($total) { $this->count = $total->item(0)->textContent; }
+       print "DEBUG: total is $this->count<br>\n";
      } else {
-       print "Transformation failed.<br>";
-       print "Error: " . xslt_error($xh) . " (error code " . xslt_errno($xh) . ")<br>";
+       print "Error! taminoConnection xml variable uninitialized.<br>";
      }
-     xslt_free($xh);
+   }
+
+
+   // transform the tamino XML with a specified stylesheet
+   function xslTransform ($xsl_file, $xsl_params = NULL) {
+     /* load xsl & xml as DOM documents */
+     $xsl = new DomDocument();
+     $xsl->load($xsl_file);
+     $xml = new DomDocument();
+     $xml->load($this->xmlContent);
+
+     /* create processor & import stylesheet */
+     $proc = new XsltProcessor();
+     $xsl = $proc->importStylesheet($xsl);
+     foreach ($xsl_params as $name => $val) {
+       $proc->setParameter(null, $name, $val);
+     }
+
+     /* transform and output the xml document */
+     $output = $proc->transformToDoc($xml);
    }
 
    function printResult ($term = NULL) {
@@ -205,4 +210,43 @@ class taminoConnection {
      }
    }
 
+
+   // create a new domDocument with the raw xmlContent, retrieve tamino messages
+   function initializeXML () {
+    $this->xml = new domDocument();
+    $this->xml->loadXML($this->xmlContent);
+    if (!$this->xml) {
+      print "TaminoConnection::xquery error: unable to parse xml content.<br>";
+      $this->xq_rval = 0;	// not a tamino error but a dom error
+    } else {
+     $this->xpath = new domxpath($this->xml);
+     // note: query returns a dome node list object
+     $nl = $this->xpath->query("/ino:response/ino:message/@ino:returnvalue");
+     if ($nl) { $this->xq_rval = $nl->item(0)->textContent; }
+     $nl = $this->xpath->query("/ino:response/ino:message/ino:messagetext/@ino:code");
+     if ($nl) { $this->xq_code = $nl->item(0)->textContent; }
+     $nl =  $this->xpath->query("/ino:response/ino:message/ino:messagetext");
+     if ($nl) { $this->xq_msg = $nl->item(0)->textContent; }
+     if ($this->debug) {
+       print "Tamino return value : $this->xq_rval<br>\n";
+       if ($this->xq_code) {
+         print "Tamino code : $this->xq_code<br>\n";
+       }
+       if ($this->xq_msg) {
+	 print "Tamino message : $this->xq_msg<br>\n";
+       }
+     }
+    }
+   }
+
+   // print out xml (for debugging purposes)
+   function displayXML () {
+     if ($this->xml) {
+       $this->xml->formatOutput = true;
+       print "<pre>";
+       print htmlentities($this->xml->saveXML());
+       print "</pre>";
+     }
+   }
+   
 }
