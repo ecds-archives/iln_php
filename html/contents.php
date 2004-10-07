1f06a77a76e7ef850a69e141f41a8863f6077964
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2004-10-07 22:00:31 +0000

    beginning to test with new taminoConnection (doesn't do xslt transform yet)

--- html/contents.php
+++ html/contents.php
@@ -3,10 +3,10 @@
 include_once("lib/taminoConnection.class.php");
 include("common_functions.php");
 
-$args = array('host' => "vip.library.emory.edu",
-		'db' => "BECKCTR",
-	      //	      'debug' => true,
-		'coll' => 'ILN');
+$args = array('host' => $tamino_server,
+	      'db' => $tamino_db,
+	      'coll' => $tamino_coll,
+	      'debug' => true);
 $tamino = new taminoConnection($args);
 
 $query = 'for $b in input()/TEI.2//div1
@@ -22,27 +22,27 @@ return <div1>
      {$c/bibl}
      { for $d in $c/p/figure return $d}
    </div2>
-}</div1>';
+}
+</div1>';
+/*
+added this to query to test taminoConnection class
+<total>{count(input()/TEI.2//div1/div2)}</total>
+*/
 
-$rval = $tamino->xquery($query);
-if ($rval) {       // tamino Error code (0 = success)
-  print "<p>Error: failed to retrieve contents.<br>";
-  print "(Tamino error code $rval)</p>";
-  exit();
-} 
 
+$tamino->xquery($query);
 
 html_head("Browse");
 
 include("xml/head.xml");
-include("xml/sidebar.xml");
+//include("xml/sidebar.xml");
 
 print '<div class="content"> 
           <h2>Browse</h2>';
 print "<hr>";
 $xsl_file = "contents.xsl";
-$tamino->xslTransform($xsl_file);
-$tamino->printResult();
+//$tamino->xslTransform($xsl_file);
+//$tamino->printResult();
 
 print "<hr>";
 
