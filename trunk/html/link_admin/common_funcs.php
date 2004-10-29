72a847f0ab1c19840f553bc6421279b69e0051bc
rsutton <rsutton@32a9c6a3-583a-0410-a6b5-ea5aef07eef3>
2004-10-29 22:13:43 +0000

    common functions for link_admin pages

---
+++ trunk/html/link_admin/common_funcs.php
@@ -0,0 +1,17 @@
+<?php
+
+// php functions & variables used by more than one ILN php page
+
+
+function link_head ($mode) {
+print "<html>
+ <head>
+ <title>$mode - The Civil War in America from The Illustrated London News</title>
+<meta http-equiv=\"Content-Type\" content=\"text/html; charset=iso-8859-1\">
+<link rel=\"stylesheet\" type=\"text/css\" href=\"http://arachne.library.emory.edu/rebecca/ilnweb/iln.css\">
+ </head>";
+}
+
+
+
+?>
