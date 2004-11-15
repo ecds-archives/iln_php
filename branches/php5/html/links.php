d0dba81da09c4132a6bc9f077ec1c53f6cd42163
rsutton <rsutton@32a9c6a3-583a-0410-a6b5-ea5aef07eef3>
2004-11-15 23:29:07 +0000

    use config variables for tamino settings

--- branches/php5/html/links.php
+++ branches/php5/html/links.php
@@ -7,63 +7,31 @@ html_head("Links");
 include("xml/head.xml");
 include("xml/sidebar.xml");
 
-//$url = 'http://tamino.library.emory.edu/passthru/servlet/transform/tamino/BECKCTR/ILN?_xquery=
-/*$url = 'http://vip.library.emory.edu/tamino/BECKCTR/iln_links?_xquery=
-declare namespace dc="http://purl.org/dc/elements/1.1/"
-for $b in input()/link_collection/link_record
-return <link_record>
- {$b/dc:title}
- {$b/dc:description}
- {$b/dc:identifier}
-</link_record>';
+print "<div class='content'>
+  <h2>Links</h2>";   
+print "<hr>"; 
 
-$url = encode_url($url);
-$xsl_file = "links.xsl"; */
+$sort = $_GET["sort"]; // options: title|contrib|date 
+$subject = $_GET['subj'];  
 
+$args = array('host' => $tamino_server,
+	      'db' => $tamino_db, 
+	      'coll' => $link_coll,  
+	      'limit_subject' => $subject[0], 
+	      'sort' => $sort,  
+	      'debug' => false);  
+
+$linkset = new LinkCollection($args);  
+
+$linkset->printSortOptions("links.php");  
+$linkset->printSubjectOptions("links.php", $subject);  
+print "<hr width='50%'>";  
+$linkset->printSummary(); 
+
+print "<hr>";  
+print "</div>";  
+include("xml/foot.xml"); 
 ?>
-
-   <div class="content"> 
-          <h2>Links</h2>
-
-
-<?php
-
-print "<hr>";
-
-// use sablotron to transform xml
-/*$xmlContent = file_get_contents($url);
-$result = transform($xmlContent, $xsl_file); 
-print $result; */
-
-$sort = $_GET["sort"]; // options: title|contrib|date
-$subject = $_GET['subj'];
-
-$args = array('host' => "vip.library.emory.edu",
-	      'db' => "BECKCTR",
-	      'coll' => 'iln_links',
-	      'limit_subject' => $subject[0],
-	      'sort' => $sort);
-
-$linkset = new LinkCollection($args);
-
-$linkset->printSortOptions("links.php");
-$linkset->printSubjectOptions("links.php", $subject);
-print "<hr width='50%'>";
-$linkset->printSummary();
-
-
-print "<hr>";
-
-?> 
-
-
-
-  </div>
-   
-<?php
-  include("xml/foot.xml");
-?>
-
 
 </body>
 </html>
