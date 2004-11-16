9f9e7bc1dd44e464f6b5230464c933fbd03fa70c
rsutton <rsutton@32a9c6a3-583a-0410-a6b5-ea5aef07eef3>
2004-11-16 00:02:05 +0000

    moved old version to new directory structure; updated - config vars for tamino settings

---
+++ branches/php5/html/admin/links/do_add.php
@@ -0,0 +1,49 @@
+<?php
+include("../../config.php");
+include("common_functions.php");
+include_once ("linkRecord.class.php");
+
+html_head("Links - Process new link");
+include("xml/head.xml");
+include("xml/sidebar.xml");
+
+print '<div class="content">
+<h2>Processing new link</h2>'; 
+include("nav.html");
+
+
+$url = htmlentities($_GET["url"]);
+$title = htmlentities($_GET["title"]);
+$description = htmlentities($_GET["desc"]);
+$subject = $_GET["subj"];
+$date = htmlentities($_GET["date"]);
+$contributor = htmlentities($_GET["contrib"]);
+
+// check that variables are set (all fields should be set)
+if (!(isset($url))||(!(isset($title)))||(!(isset($description)))
+    ||(!isset($subject))||(!isset($date))||(!isset($contributor))) {
+  print "<p class='error'>Error! One or more required fields were not defined.</p>";
+  print "Please <a href='javascript:back()'>go back</a> and fill in those fields.";
+  exit();
+}
+
+$myargs = array('host' => $tamino_server,
+		'db' => $tamino_db,
+		'coll' => $link_coll,
+		'url' => $url,
+		'title' => $title,
+		'description' => $description,
+		'date' => $date,
+		'contributor' => $contributor,
+		'debug' => false);
+$newlink = new LinkRecord($myargs, $subject);
+$newlink->taminoAdd(); 
+$newlink->printHTML(); 
+
+include("nav.html");
+
+print '</div>';
+include("xml/foot.xml");
+?>
+</body>
+</html>
