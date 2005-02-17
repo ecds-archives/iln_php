fdfcce10af77518c623ced7978359152d49260bb
Rebecca Sutton Koeser <rebecca.s.koeser@emory.edu>
2005-02-17 00:05:03 +0000

    split out tamino functionality, made a more general class to handle xslt, etc.

--- html/lib/taminoConnection.class.php
+++ html/lib/taminoConnection.class.php
@@ -27,8 +27,6 @@ class taminoConnection {
   // variables for highlighting search terms
   var $begin_hi;
   var $end_hi;
-
-
   
 
   function taminoConnection($argArray) {
@@ -49,7 +47,7 @@ class taminoConnection {
   // send an xquery to tamino & get xml result
   // returns  tamino error code (0 for success, non-zero for failure)
   function xquery ($query, $position = NULL, $maxdisplay = NULL) {
-    $myurl = $this->base_url . "_xquery=" . $this->encode_xquery($query) . "&_encoding=utf-8";
+    $myurl = $this->base_url . "_xquery=$query&_encoding=utf-8";
     if (isset($position) && isset($maxdisplay)) {
       $myurl .= "&_cursor=open&_position=$position&_quantity=$maxdisplay&_sensitive=vague";
     }
@@ -100,7 +98,7 @@ class taminoConnection {
     } else {
       $xql = "_xql=";
     }
-    $myurl = $this->base_url . $xql . $this->encode_xquery($query);
+    $myurl = $this->base_url . $xql . $query;
     if ($this->debug) {
       print "DEBUG: In function taminoConnection::xql, url is $myurl.<p>";
     }
@@ -119,15 +117,6 @@ class taminoConnection {
     return $this->xq_rval;
   }
 
-   // convert a readable xquery into a clean url for tamino
-   function encode_xquery ($string) {
-     // get rid of multiple white spaces
-     $string = preg_replace("/\s+/", " ", $string);
-     // convert spaces to their hex equivalent
-     $string = str_replace(" ", "%20", $string);
-     return $string;
-   }
-
    // retrieve the cursor & get the total count
    function getCursor () {
      // NOTE: this is an xql style cursor, not xquery
@@ -138,7 +127,6 @@ class taminoConnection {
        print "Error! taminoConnection xml variable uninitialized.<br>";
      }
    }
-
 
       // retrieve the XQuery style cursor & get the total count
    function getXQueryCursor () {
@@ -152,87 +140,6 @@ class taminoConnection {
        if ($total) { $this->count = $total->item(0)->textContent; }
      } else {
        print "Error! taminoConnection xml variable uninitialized.<br>";
-     }
-   }
-
-
-   // transform the tamino XML with a specified stylesheet
-   function xslTransform ($xsl_file, $xsl_params = NULL) {
-
-     /* load xsl & xml as DOM documents */
-     $xsl = new DomDocument();
-     $xsl->load("xsl/$xsl_file");
-
-     /* create processor & import stylesheet */
-     $proc = new XsltProcessor();
-     $xsl = $proc->importStylesheet($xsl);
-     if ($xsl_params) {
-       foreach ($xsl_params as $name => $val) {
-         $proc->setParameter(null, $name, $val);
-       }
-     }
-     /* transform the xml document and store the result */
-     $this->xsl_result = $proc->transformToDoc($this->xml);
-   }
-
-   function printResult ($term = NULL) {
-     if ($this->xsl_result) {
-       if (isset($term[0])) {
-         $this->highlightXML($term);
-         // this is a bit of a hack: the <span> tags used for
-         // highlighting are strings, and not structural xml; this
-         // allows them to display properly, rather than with &gt; and
-         // &lt; entities
-         print html_entity_decode($this->xsl_result->saveXML());
-       } else {
-         print $this->xsl_result->saveXML();
-       }
-     }
-
-   }
-
-   // Highlight the search strings within the xsl transformed result.
-   // Takes an array of terms to highlight.
-   function highlightString ($str, $term) {
-     // note: need to fix regexps: * -> \w* (any word character)
-      // FIXME: how best to deal with wild cards?
-
-     // only do highlighting if the term is defined
-     for ($i = 0; (isset($term[$i]) && ($term[$i] != '')); $i++) {
-       // replace tamino wildcard (*) with regexp -- 1 or more word characters 
-       $_term = str_replace("*", "\w+", $term[$i]);
-     // Note: regexp is constructed to avoid matching/highlighting the terms in a url 
-       $str = preg_replace("/([^=|']\b)($_term)(\b)/i",
-	      "$1" . $this->begin_hi[$i] . "$2$this->end_hi$3", $str);
-     }
-     return $str;
-   }
-
-   // highlight text in the xml structure
-   function highlightXML ($term) {
-     $this->highlight_node($this->xsl_result, $term);
-   }
-
-   // recursive function to highlight search terms in xml text
-   function highlight_node ($n, $term) {
-     $children = $n->childNodes;
-     foreach ($children as $c) {
-       if ($c instanceof domElement) {
-	 $this->highlight_node($c, $term);
-       } else if ($c instanceof DOMCharacterData) {
-	 $c->nodeValue = $this->highlightString($c->nodeValue, $term);
-       }
-     }
-   }
-   
-   // print out search terms, with highlighting matching that in the text
-   function highlightInfo ($term) {
-     if (isset($term[0])) {
-       print "<p align='center'>The following search terms have been highlighted: ";
-       for ($i = 0; isset($term[$i]); $i++) {
-	 print "&nbsp; " . $this->begin_hi[$i] . "$term[$i]$this->end_hi &nbsp;";
-       }
-       print "</p>";
      }
    }
 
