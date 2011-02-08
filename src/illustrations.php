d619ff768ab97d534212859e479fc59482dc75ff
lshanna <lshanna@32a9c6a3-583a-0410-a6b5-ea5aef07eef3>
2011-02-08 20:11:13 +0000

    adding tei:

--- src/illustrations.php
+++ src/illustrations.php
@@ -7,17 +7,17 @@ include("common_functions.php");
 $exist_args{"debug"} =false;
 $xmldb = new xmlDbConnection($exist_args);
 
-$query = 'for $b in /TEI.2/text/body/div1
+$query = 'for $b in /tei:TEI/tei:text/tei:body/tei:div1
 order by $b/@id
 return <div1 type="{$b/@type}">
- {$b/head}
- {$b/docDate}
-{for $art in $b/div2[.//figure]
-let $fig := $art//figure
+ {$b/tei:head}
+ {$b/tei:docDate}
+{for $art in $b/tei:div2[.//tei:figure]
+let $fig := $art//tei:figure
 return
 <div2>
- {$art/head}
- {$art/bibl}
+ {$art/tei:head}
+ {$art/tei:bibl}
  {$fig}
 </div2>}
 </div1>';
@@ -27,9 +27,9 @@ $xmldb->xquery($query);
 
 
 html_head("Browse - Illustrations", true);
-print '</head>';
-include("web/xml/head.xml");
-include("web/xml/sidebar.xml");
+print '</tei:head>';
+include("web/xml/tei:head.xml");
+include("web/xml/tei:sidebar.xml");
 
 print '<div class="content"> 
       <h2>Illustrations</h2>';
